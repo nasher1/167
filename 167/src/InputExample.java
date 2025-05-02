8d74bf377ee171af636f682ea1da9b2f8a78af88
nasher1 <138151716+nasher1@users.noreply.github.com>
2025-05-02 16:49:59 -0400

    Initial Commit

---
+++ 167/src/InputExample.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class InputExample {
+   public static void main(String [] args) {
+      Scanner scnr = new Scanner(System.in);
+      int birthMonth;
+      int birthYear;
+
+      /* Your solution goes here  */
+      birthMonth = scnr.nextInt();
+      
+      birthYear = scnr.nextInt();
+      
+      System.out.print( birthMonth+ "/" +birthYear );
+      
+      
+
+   }
+}
